6572b6963365ce6006537947fef800b293068b6a
AnthonyJeria <anth.jeria@duocuc.cl>
2023-09-02 13:14:14 -0400

    Validacion del login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,6 +5,7 @@ import { IonicModule } from '@ionic/angular';
 import { UserModel } from '../models/UserModel';
 import { IUserLogin } from '../models/IUserLogin';
 import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -25,23 +26,57 @@ export class LoginPage implements OnInit {
     password: ''
   };
 
-  constructor(private route: Router) { }
+  constructor(private route: Router, private alertController: AlertController) { }
 
   ngOnInit() {
     this.userLoginModalRestart()
   }
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Email o contraseña incorrecta',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async presentAlert2() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Debe llenar todos los campos',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  validar = false;
+
   userLogin(userLoginInfo: IUserLogin): boolean{
-    for(let i = 0; i < this.listUser.length; i++){
-      if((this.listUser[i].email == userLoginInfo.email) && (this.listUser[i].password == userLoginInfo.password)){
-        console.log('User Loged...', this.userLoginModal.email, this.userLoginModal.password);
-        let userInfoSend: NavigationExtras = {
-          state: {
-            user: this.listUser[i]
+
+    if ((userLoginInfo.email == "") || (userLoginInfo.password =="")) {
+      this.presentAlert2();
+    }else{
+
+      for(let i = 0; i < this.listUser.length; i++){
+        if((this.listUser[i].email == userLoginInfo.email) && (this.listUser[i].password == userLoginInfo.password)){
+          this.validar = true;
+          console.log('User Loged...', this.userLoginModal.email, this.userLoginModal.password);
+          let userInfoSend: NavigationExtras = {
+            state: {
+              user: this.listUser[i]
+            }
           }
+          let sendInfo = this.route.navigate(['/vista-alumno'], userInfoSend);
         }
-        let sendInfo = this.route.navigate(['/vista-alumno'], userInfoSend);
       }
+  
+      if (this.validar == false) {
+        this.presentAlert();
+      }
+
     }
 
     this.userLoginModalRestart();
